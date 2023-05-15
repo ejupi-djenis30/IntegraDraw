5c4fba5acf2515554f7497780f698af06f20eab4
Djenis Ejupi <ejupi.djenis03@gmail.com>
2023-05-15 19:33:38 +0200

    boh

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -1,7 +1,28 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.math.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    print("Inserisci una funzione (utilizza 'x' come variabile): ")
+    val input = readLine() ?: return
+    plotFunction(input)
+}
+
+fun plotFunction(function: String) {
+    val range = -10.0..10.0
+    val step = 0.1
+
+    for (x in range step step) {
+        val y = evaluateFunction(function, x)
+        val height = (y / 2).toInt()
+        val line = " ".repeat(50 + height) + "*"
+        println(line)
+    }
+}
+
+fun evaluateFunction(function: String, x: Double): Double {
+    val expression = function.replace("x", "($x)")
+    return try {
+        ScriptEngineManager().getEngineByName("js").eval(expression) as Double
+    } catch (e: Exception) {
+        Double.NaN
+    }
 }
