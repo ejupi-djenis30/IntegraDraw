34e7247da885127a4da103b817b56edfa72c674e
NobodyToListen <giacalone293@gmail.com>
2023-05-16 18:15:20 +0200

    Creata interfaccia

---
+++ src/main/java/com/planck/ui/widgets/GraphView.java
@@ -0,0 +1,12 @@
+package com.planck.ui.widgets;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphView extends JPanel {
+    public GraphView() {
+        super();
+
+        setBackground(Color.WHITE);
+    }
+}
