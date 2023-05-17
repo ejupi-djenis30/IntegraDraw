912a3793ba143294d35678a5ceb02f4ba1399441
Project Contributor <project-contributor@example.invalid>
2023-05-17 13:49:57 +0200

    Adesso la formula dovrebbe essere visualizzata correttamente (in teoria)

--- src/main/java/com/planck/ui/widgets/GraphView.java
+++ src/main/java/com/planck/ui/widgets/GraphView.java
@@ -6,9 +6,10 @@ import net.objecthunter.exp4j.ExpressionBuilder;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Objects;
 
 public class GraphView extends JPanel {
+    private static final int LIMIT = 250;
+
     public GraphView() {
         super();
 
@@ -51,13 +52,62 @@ public class GraphView extends JPanel {
         if (formula.isEmpty())
             return;
 
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i <= LIMIT; i++) {
+            int[] segment = getFormulaSegment(i, formula, 1);
+            segment[0] += LIMIT;
+            segment[1] += LIMIT;
+
+            g.drawLine(i + LIMIT, segment[0], i + 1 + LIMIT, segment[1]);
+        }
+
+        g.setColor(Color.RED);
+        for (int i = 0; i >= -LIMIT; i--) {
+            int[] segment = getFormulaSegment(i, formula, -1);
+            segment[0] += LIMIT;
+            segment[1] += LIMIT;
+
+            g.drawLine(i + LIMIT, segment[0], i - 1 + LIMIT, segment[1]);
+        }
+
+        /*
+        for (int i = 250; i >= 0; i--) {
+            g.setColor(Color.RED);
+            drawFormulaSegment(i, formula, g, -1);
+        }
+
+        for (int i = 250; i <= LIMIT * 2; i++) {
+            g.setColor(Color.BLUE);
+            drawFormulaSegment(i, formula, g, 1);
+        }*/
+/*
+        for (int i = -LIMIT; i <= LIMIT * 2; i++) {
             Expression expression = new ExpressionBuilder(formula)
                     .variables("x").build()
                     .setVariable("x", i);
-            int result = (int) expression.evaluate();
 
-            g.drawLine(i, result, i, result);
-        }
+            Expression expression_after = new ExpressionBuilder(formula)
+                    .variables("x").build()
+                    .setVariable("x", i + LIMIT + 1);
+
+            int result = (int) expression.evaluate();
+            int result_after = (int) expression_after.evaluate();
+
+            g.drawLine(i, result, i + 1, result_after);
+        }*/
+    }
+
+    private int[] getFormulaSegment(int i, String formula, int inc) {
+        Expression expression = new ExpressionBuilder(formula)
+                .variables("x").build()
+                .setVariable("x", i);
+
+        Expression expression_after = new ExpressionBuilder(formula)
+                .variables("x").build()
+                .setVariable("x", i + inc);
+
+        int result = (int) expression.evaluate();
+        int result_after = (int) expression_after.evaluate();
+
+        return new int[]{result, result_after};
     }
 }
