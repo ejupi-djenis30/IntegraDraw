df6d4b491882516330a15ba0f7eed915e0ec9561
NobodyToListen <giacalone293@gmail.com>
2023-05-17 10:22:29 +0200

    Adesso nel panel vengono disegnati gli assi dell'asse cartesiano

--- src/main/java/com/planck/ui/widgets/GraphView.java
+++ src/main/java/com/planck/ui/widgets/GraphView.java
@@ -9,4 +9,17 @@ public class GraphView extends JPanel {
 
         setBackground(Color.WHITE);
     }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawPlane(g);
+    }
+
+    private void drawPlane(Graphics g) {
+        g.setColor(Color.BLACK);
+
+        g.drawLine(0, 250, 500, 250);
+        g.drawLine(250, 0, 250, 500);
+    }
 }
