ca2ad1c5446ea419fb2aa4bbc485667164bda479
Project Contributor <project-contributor@example.invalid>
2023-05-17 10:28:56 +0200

    Adesso nel panel vengono disegnate le frecce

--- src/main/java/com/planck/ui/widgets/GraphView.java
+++ src/main/java/com/planck/ui/widgets/GraphView.java
@@ -13,13 +13,27 @@ public class GraphView extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        drawPlane(g);
+        drawAxis(g);
     }
 
-    private void drawPlane(Graphics g) {
+    private void drawAxis(Graphics g) {
         g.setColor(Color.BLACK);
 
+        // Disegnare gli assi
         g.drawLine(0, 250, 500, 250);
         g.drawLine(250, 0, 250, 500);
+
+        // Disegnare le frecce
+        drawArrows(g);
+    }
+
+    private void drawArrows(Graphics g) {
+        // Disegnare freccia X.
+        g.drawLine(500, 250, 490, 240);
+        g.drawLine(500, 250, 490, 260);
+
+        // Disegnare freccia Y.
+        g.drawLine(250, 0, 260, 10);
+        g.drawLine(250, 0, 240, 10);
     }
 }
