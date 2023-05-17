e7968d958efabeaf9d36ea2349bfe1c978191fc8
Djenis Ejupi <ejupi.djenis03@gmail.com>
2023-05-17 14:35:25 +0200

    matematica sviluppata

---
+++ src/main/java/com/planck/math/Rectangle.java
@@ -0,0 +1,31 @@
+package com.planck.math;
+
+public class Rectangle {
+    private double x;
+    private double height;
+    private double width;
+    private double area;
+
+    public Rectangle(double x,double height, double width) {
+        this.x = x;
+        this.height = height;
+        this.width = width;
+        this.area = this.width * this.height;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getArea() {
+        return area;
+    }
+}
